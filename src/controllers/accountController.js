30f3c0883b1068a23085ac5290557eef60282be5
ochosteve <ochosteve08@gmail.com>
2023-11-25 04:05:49 +0100

    create controllers

---
+++ src/controllers/accountController.js
@@ -0,0 +1,6 @@
+const { db } = require('../index')
+const { errorHandler } = require('../utils/errorHandler')
+
+const updateAccount = async (req, res, next) => {}
+
+module.exports = { updateAccount }
